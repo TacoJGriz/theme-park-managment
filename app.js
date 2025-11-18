6e3bcbb3c2a874a7a8f62f13ef432debfaf6f3fd
Aurelius-HUB <curaturae@gmail.com>
2025-11-18 01:39:08 -0600

    home page enhance

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ const {
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 // --- SESSION CONFIGURATION ---
 app.use(session({
